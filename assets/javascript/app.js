0a20dbfe11e8b18d03ab2c138835d8d7c37557f9
Ciwonie <ciwonie@gmail.com>
2018-07-12 15:19:34 -0500

    api update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,73 +1,65 @@
 
 $(document).ready(function () {
     console.log("ready!");
-});
 
-var queryURL;
-var key = 'hQF91TC0yyfVRfg6eKOdBe95T4NyI02X';
-
-var topics = [
-    'Captain America',
-    'Spider-Man',
-    'Iron-Man',
-    'Thanos',
-    'Black Panther',
-    'Groot',
-    'Drax',
-    'Captain Marvel',
-    'Hulk',
-    'Thor'
-]
-
-renderButtons();
-function renderButtons() {
-    $('.buttons').empty();
-    for (var i = 0; i < topics.length; i++) {
-        var button = $('<button>');
-        button.addClass('gifs');
-        button.attr('data-value', topics[i].replace(/\s+/g, '+'));
-        button.text(topics[i]);
-        $('.buttons').append(button);
-    }
-}
-
-$("#add-gif").on("click", function(event) {
-    event.preventDefault();
-    
-    var gifInput = $("#giphy-input").val().trim();
-
-    topics.push(gifInput);
+    var topics = [
+        'Captain America',
+        'Spider-Man',
+        'Iron-Man',
+        'Thanos',
+        'Black Panther',
+        'Groot',
+        'Drax',
+        'Captain Marvel',
+        'Hulk',
+        'Thor'
+    ]
 
     renderButtons();
-});
+    function renderButtons() {
+        $('.buttons').empty();
+        for (var i = 0; i < topics.length; i++) {
+            var button = $('<button>');
+            button.addClass('gifs');
+            button.attr('data-value', topics[i].replace(/\s+/g, '+'));
+            button.text(topics[i]);
+            $('.buttons').append(button);
+        }
+    }
+
+    $("#add-gif").on("click", function (event) {
+        event.preventDefault();
+
+        var gifInput = $("#giphy-input").val().trim();
+
+        topics.push(gifInput);
+
+        renderButtons();
+    });
 
 
-$(document).on('click', '.gifs', function () {
+    $(document).on('click', '.gifs', function () {
 
-    var gif = $(this).attr('data-value')
-    console.log(gif);
-    queryURL = 'https://api.giphy.com/v1/gifs/random?api_key=hQF91TC0yyfVRfg6eKOdBe95T4NyI02X&limit=10&tag=' + gif;
+        var gif = $(this).attr('data-value')
+        console.log(gif);
+        queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=hQF91TC0yyfVRfg6eKOdBe95T4NyI02X&q=' + gif + '&limit=10&offset=0&rating=&lang=en';
+        $.ajax ({
+            url: queryURL,
+            method: 'GET'
+        }).then(function (response) {
+                var gifURL = response.data.image_original.url;
 
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-    })
+                var gifImage = $('<img>');
 
-        // promise
-        .then(function (response) {
-        var gifURL = response.data.image_original.url;
+                gifImage.attr('src', gifURL);
+                gifImage.attr('alt', gif);
 
-        var gifImage = $('<img>');
+                $('.gifs').prepend(gifImage);
+            });
 
-        gifImage.attr('src', gifURL);
-        gifImage.attr('alt', gif);
 
-        $('.gifs').prepend(gifImage);
     });
 
 
 });
-
-
-
 
